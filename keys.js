d34c3dd02bc58fff5e524ce1cff3a8f8c0cccd26
Rebekah <rebmid@gmail.com>
2018-10-02 15:13:51 -0700

    tested post and get

--- keys.js
+++ keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     mongoURI:
-    'mongodb://rebmidadmin:9920mae@ds014658.mlab.com:14658/socialconnector'
+    'mongodb://rebmidadmin:9920mae@ds014658.mlab.com:14658/socialconnector', 
+    secretOrKey: 'secret'
 }
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 const app = express();
+
+//Body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 //DB config
 const db = require('./keys').mongoURI;
@@ -13,14 +19,18 @@ mongoose
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log(err));
 
-//Our first route
-app.get('/', (req, res) => res.send('Hello'));
+//Passport middleware
+app.use(passport.initialize());
+
+//Passport config
+require('./config/passport')(passport);
+
 
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
 
-const port = 7600;
+const port = 7000;
 app.listen(port,() => console.log(`Server running 
 on port ${port}`) );
