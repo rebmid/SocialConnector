4873d0d5d0029f2ebd0dda0fde61105c25e3ca1a
Rebekah <rebmid@gmail.com>
2018-10-12 19:18:15 -0700

    Rebekah

--- config/passport.js
+++ config/passport.js
@@ -6,6 +6,7 @@ const keys = require('../keys');
 
 const opts = {};
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+
 opts.secretOrKey = keys.secretOrKey;
 
 module.exports = passport => {
