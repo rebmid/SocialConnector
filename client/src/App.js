4c8680082fe66f2aa94dca47c9e6be6730995eed
Rebekah <rebmid@gmail.com>
2018-10-23 18:26:10 -0700

    module 17 class added

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,27 @@ import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
-
+import {BrowserRouter as Router, Route} from 
+'react-router-dom';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register'
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
       <Navbar />
-      <Landing />
+      <Route exact path="/" component={Landing} />
+      <div className="container">
+       <Route exact path="/register" component={Register} />
+       <Route exact path="/login" component={Login} />
+       </div>
       <Footer />
       </div>
+      </Router>
     );
   }
 }
